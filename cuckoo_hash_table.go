ef178a12e0edecb8ddb92eab35daae4e0e169fb2
Dave King <dave.king@rackspace.com>
2014-06-07 09:21:30 -0400

    Random hash functions for cuckoo hashing

--- cuckoo_hash_table.go
+++ cuckoo_hash_table.go
@@ -1,16 +1,60 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type CuckooHashTable struct {
 	table []Element
+
+	rand  *rand.Rand
 	func1 HashFunc
 	func2 HashFunc
 }
 
-func New(size int, func1 HashFunc, func2 HashFunc) *CuckooHashTable {
+func log2(size int) uint {
+	r := 0
+	for size != 0 {
+		size >>= 1
+		r++
+	}
+
+	// Number of iterations to reach 0 value minus 1, e.g. log2(1) = 0
+	return uint(r - 1)
+}
+
+func generateHashFunction(r *rand.Rand, size int) HashFunc {
+	// From http://www.keithschwarz.com/interesting/code/cuckoo-hashmap/CuckooHashMap.java.html
+	// Universal hash family
+	//    HashCode = ((HIGH + A) * (LOW * B)) / (2^(32 - k))
+	// For A, B random
+
+	return func(elem Element) int {
+		value := elem.Value.(int32)
+		high := value >> 16
+		low := value & 0x0000FFFF
+		A := r.Int31()
+		B := r.Int31()
+
+		return int((high + A) + (low+B)>>(2^(32-log2(size))))
+	}
+}
+
+func New(size int, seed int64) *CuckooHashTable {
+	var r *rand.Rand
+
+	if seed == 0 {
+		seed = time.Now().UnixNano()
+	}
+	r = rand.New(rand.NewSource(seed))
+
+	func1, func2 := generateHashFunction(r, size), generateHashFunction(r, size)
 	table := CuckooHashTable{
 		table: make([]Element, size),
 		func1: func1,
 		func2: func2,
+		rand:  r,
 	}
 	return &table
 }
--- cuckoo_hash_table_test.go
+++ cuckoo_hash_table_test.go
@@ -14,8 +14,21 @@ func two(elem Element) int {
 	return (k / 11) % 11
 }
 
+func TestLog2(t *testing.T) {
+	if log2(8) != 3 {
+		t.Errorf("Wrong log2 of 8, %d", log2(8))
+	}
+
+	if log2(16) != 4 {
+		t.Errorf("Wrong log2 of 16, %d", log2(16))
+	}
+}
+
 func TestNew(t *testing.T) {
-	table := New(1000, one, two)
+	table := New(1000, 0)
+	table.func1 = one
+	table.func2 = two
+
 	if s := table.Size(); s != 1000 {
 		t.Errorf("Table was not expected size.")
 	}
